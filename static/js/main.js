aee628a66016df0b88322d38394643df115a3c04
nicolastoon <nicolastoon05@gmail.com>
2026-02-20 11:25:45 -0800

    fixing dropdown interaction

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 const dropdown = document.getElementById("gene-of-interest-dropdown");
 const field = document.getElementById("gene-of-interest-name");
 
-selectElement.addEventListener('change', (e) => {
+dropdown.addEventListener('change', (e) => {
   const selectedValue = e.target.value;
   field.textContent = selectedValue;
 });
